623fc241622ff212161ddfbf3ca698c7384c0636
Suryakumar Selvakumar <ssuryakumar2077@gmail.com>
2025-09-09 20:18:00 -0600

    chore: Remove commented code

---
+++ Tic-Tac-Toe with AI (Java)/task/src/player/Player.java
@@ -0,0 +1,13 @@
+package player;
+
+public class Player {
+    private final char marker;
+
+    public Player(char marker) {
+        this.marker = marker;
+    }
+
+    public char getMarker() {
+        return marker;
+    }
+}
